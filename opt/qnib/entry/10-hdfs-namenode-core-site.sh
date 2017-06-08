0c3133f2e5d2c5e350729770498546e2574fac38
Christian Kniep <christian@qnib.org>
2017-06-08 22:37:47 +0200

    IPs are reverse lookup to hdfs_namenode...

--- opt/qnib/entry/10-hdfs-namenode-core-site.sh
+++ opt/qnib/entry/10-hdfs-namenode-core-site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
-
+#HDFS_NAMENODE_LIST=$(go-fisherman --out list --template "hdfs://{{.IP}}:8020" namenode)
+HDFS_NAMENODE_LIST="hdfs://tasks.namenode:8020"
 cat /opt/qnib/hdfs/etc/core-site.xml \
- > /opt/hadoop/etc/hadoop/core-site.xml
+  |sed -e "s#HDFS_NAMENODE_LIST#${HDFS_NAMENODE_LIST}#" \
+  > /opt/hadoop/etc/hadoop/core-site.xml
