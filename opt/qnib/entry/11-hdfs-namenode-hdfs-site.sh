46765f8c13036175f63f1d4ab4f3a0ccca04f3a8
Christian Kniep <christian@qnib.org>
2017-06-08 18:00:58 +0200

    use go-fisherman

--- opt/qnib/entry/11-hdfs-namenode-hdfs-site.sh
+++ opt/qnib/entry/11-hdfs-namenode-hdfs-site.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
-
+HDFS_NAMENODE_LIST=$(go-fisherman --out list --template "hdfs://{{.IP}}:8020" namenode)
 cat /opt/qnib/hdfs/etc/hdfs-site.xml \
+ |sed -i'' -e "s/HDFS_NAMENODE_LIST/${HDFS_NAMENODE_LIST}/" \
  > /opt/hadoop/etc/hadoop/hdfs-site.xml
